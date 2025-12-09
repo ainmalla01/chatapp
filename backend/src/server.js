84555d5e74a0c36dad3eeb2f25809cbe90e01d6e
Ain Bahadur Malla <109906397+ainmalla01@users.noreply.github.com>
2025-12-10 02:47:33 +0545

    ready for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
@@ -12,11 +13,21 @@ const Port=process.env.PORT||3000;
 // middleware
 app.use(express.json());
 app.use(cors());
-
-
+// for developyment
+const __dirname=path.resolve();
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
+
+// make ready for deployment
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+}
     
 
 app.listen(Port,()=>{
